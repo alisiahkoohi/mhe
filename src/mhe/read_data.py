be980e8977385f46e74a8fb524ad563568d4be4a
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2020-02-07 15:16:23 -0500

    fix

--- src/mhe/read_data.py
+++ src/mhe/read_data.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-def my_read(fileName, n1, n2):
-    data = np.fromfile(fileName,
-                dtype='>f4', sep="")
+def my_read(fileName):
+    data = np.fromfile(fileName, dtype='>f4', sep="")
 
     return data
 
@@ -15,8 +14,9 @@ if __name__ == '__main__':
     name = "tp73.dat"
     fileName = os.path.join(_dataDir, seismicDir, name)
 
+    data = my_read(fileName)
+
     n1,n2 = 240, 357
-    data = my_read(fileName, n1, n2)
-    plt.imshow(np.reshape(data, (357, -1)).T);plt.show()
+    plt.imshow(np.reshape(data, (n2, -1)).T);plt.show()
 
 
